97b0def1051da382f565e0d76c9d1fcc0d7dca81
Uday Jain <udayjain@gmail.com>
2013-07-08 15:59:19 +0000

    Added web.js with html read

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var mybuff = fs.readFileSync('index.html');
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    response.send(mybuff.toString());
 });
 
 var port = process.env.PORT || 5000;
